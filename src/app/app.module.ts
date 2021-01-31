d6e134217324ece01c886331fedb17cfe89c6031
Farid Rico <farid.rico@imagemaker.com>
2021-01-31 11:42:07 -0500

    feat: added post layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FontAwesomeModule, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {faFacebook, faFacebookMessenger} from '@fortawesome/free-brands-svg-icons';
+import {faBell, faPlus, faAngleDown} from '@fortawesome/free-solid-svg-icons';
+
+// Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+
+const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatCardModule,
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
@@ -10,9 +28,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    FlexLayoutModule,
+    ...MATERIAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add multiple icons to the library
+    library.addIcons(
+      faFacebook,
+      faFacebookMessenger,
+      faBell,
+      faPlus,
+      faAngleDown
+    );
+  }
+}
